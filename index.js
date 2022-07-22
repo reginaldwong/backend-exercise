7a201edce4ef3237bc980ef499cf8b78b4e656b9
Reginald Wong <reginald_wong@outlook.com>
2022-07-22 10:12:08 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+let notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2022-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only Javascript',
+    date: '2022-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2022-05-30T19:20:14.298Z',
+    important: true,
+  },
+];
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  // need to use Number function since req.params.id is a string and note.id is an integer
+  const id = Number(req.params.id);
+  const note = notes.find(note => note.id === id);
+
+  if (note) {
+    res.json(note);
+  } else {
+    res.status(404).end();
+  }
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
